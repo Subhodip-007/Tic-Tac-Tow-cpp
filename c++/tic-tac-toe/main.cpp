8586572fb9555fcbd1647d0bba1e45ef234e5751
Subhodip Chatterjee <subhodip31xd@gmail.com>
2026-03-12 13:34:16 +0530

    added game logic...

--- c++/tic-tac-toe/main.cpp
+++ c++/tic-tac-toe/main.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
 using namespace std;
- void print(int matrix[3][3]){
+ bool replaceValueOfposition(int val,char matrix[3][3],char choice){
+    if(val < 1 || val > 9){
+        cout<<"enter move in range";
+        return false;
+    }
+    int row=(val-1)/3;
+    int col=(val-1)%3;
+    if(matrix[row][col]!='O' && matrix[row][col]!='X'){
+    matrix[row][col]=choice;
+    return true;
+ }
+ else{
+    cout<<"position already occupied"<<endl;
+    return false;
+ }
+}
+ void print(char matrix[3][3]){
         for(int i=0;i<3;i++){
        for(int j = 0; j < 3; j++) {   
-            cout<< " | "<< matrix[i][j] << " | ";
+            cout<< " "<<matrix[i][j];
+            if(j<2){
+                cout<<" | ";
+            }
+
+            
         }
         
         cout << endl; 
-        cout<<"---------------------"<<endl;
+        if(i<2){ cout<<"--------------"<<endl;}
+       
 }
     }
 int main(){
+    int val;
+    char choice;
    
-int board[3][3]={
-    {1,2,3},
-    {4,5,6},
-    {7,8,9},
+char board[3][3]={
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'},
 };
+
 print(board);
+cout<<"enter your position :"<<endl;
+cin>>val;
+cout<<"enter your  :"<<endl;
+
 
 }
+
+
+
+
